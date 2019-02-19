23c7bc26628b5d5250f4221dd1d9de7ca0f266e9
Seyfire <stephen.f.hoffman@gmail.com>
2019-02-19 09:33:51 -0600

    add small changes

--- ps3_hangman.py
+++ ps3_hangman.py
@@ -1,7 +1,5 @@
 # Hangman game
 # -----------------------------------
-
-
 import random
 import string
 
@@ -82,20 +80,7 @@ def getAvailableLetters(lettersGuessed):
 def hangman(secretWord):
     '''
     secretWord: string, the secret word to guess.
-
     Starts up an interactive game of Hangman.
-
-    * At the start of the game, let the user know how many 
-      letters the secretWord contains.
-
-    * Ask the user to supply one guess (i.e. letter) per round.
-
-    * The user should receive feedback immediately after each guess 
-      about whether their guess appears in the computers word.
-
-    * After each round, you should also display to the user the 
-      partially guessed word so far, as well as letters that the 
-      user has not yet guessed.
     '''
     def is_guess_correct(guess, secretWord):
         '''
@@ -107,7 +92,6 @@ def hangman(secretWord):
             return True
         else:
             return False
-
 
     print('Welcome to the game, Hangman!')
     print('I am thinking of a word that is ' + str(len(secretWord)) + ' letters long.\n-----------')
